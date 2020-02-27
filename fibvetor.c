f3fd08fb4463a4c948e2c61dd490488b82f6cb32
Robson Lima <rsl.robson@gmail.com>
2020-02-27 17:45:59 -0300

    Demonstração da quantidade de execuções recursivas

--- fibvetor.c
+++ fibvetor.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 char buf[10009];
+int cnt[70];
 
-int calc_fibonacci (int n) {	
+int calc_fibonacci (int n) {
+	cnt[n]++;
 	if (n == 0) {
         return 0;
     } else if (n == 1) {
@@ -27,6 +30,11 @@ int main () {
 			
 			int result = calc_fibonacci(val);
 			printf("Fib(%d) = %d\n", val, result);
+			
+			int i = 0;
+			for (i = 0; i <= val; i++) {
+            	printf("Calculei o fibonacci(%d) %d vezes!\n", i, cnt[i]);
+        	}
 		}
 	}
 	
